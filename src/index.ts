2f4e2522972d8403a7ad631f36ab1e35da25e97e
cyamy <hi@cyamy.dev>
2022-07-16 22:24:41 +0900

    feat: using purgecss

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import * as glob from 'glob';
-import {
-  getAllScssTopSelectors,
-  getAllHTMLSelectors,
-  getNotExist,
-} from './functions';
+import { PurgeCSS } from 'purgecss';
+import * as util from 'util';
 
-const main = () => {
+const main = async () => {
   const [, , firstArg, secondArg] = process.argv;
 
   if (!firstArg) {
@@ -26,22 +20,15 @@ const main = () => {
     process.exit(1);
   }
 
-  const htmlDir = firstArg;
-  const css = fs.readFileSync(secondArg, 'utf-8');
+  const purgeCSSResult = await new PurgeCSS().purge({
+    content: [firstArg],
+    css: [secondArg],
+    rejected: true,
+  });
 
-  const htmlFilesPath = glob.sync(path.resolve(htmlDir) + '/**/*.+' + '(html)');
-  const htmlFiles = htmlFilesPath.map((path) => fs.readFileSync(path, 'utf-8'));
-
-  const htmlSelectorList = htmlFiles.reduce((acc, cur) => {
-    return [...acc, ...getAllHTMLSelectors(cur)];
-  }, [] as string[]);
-
-  const cssSelectorList = getAllScssTopSelectors(css);
-
-  // CSS Selector のみに存在するデータの配列を生成
-  const result = getNotExist(htmlSelectorList, cssSelectorList);
-
-  console.log(result);
+  purgeCSSResult.forEach((current) => {
+    console.log(util.inspect(current.rejected, { maxArrayLength: null }));
+  });
 };
 
 main();
